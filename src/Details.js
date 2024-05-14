a93ef0e535afa2aac0bb9d6eb587c0af62c031a0
Fabien Battais <fab.battais@gmail.com>
2024-05-14 18:14:36 +0200

    first commit

---
+++ src/Details.js
@@ -0,0 +1,161 @@
+// Enter all your detials in this file
+// Logo images
+import logogradient from "./assets/logo.svg";
+import logo from "./assets/logo2.svg";
+// Profile Image
+import profile from "./assets/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import bootstrap from "./assets/techstack/bootstrap.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+import figma from "./assets/techstack/figma.png";
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+// Logos
+export const logos = {
+  logogradient: logogradient,
+  logo: logo,
+};
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Battais Fabien",
+  tagline: "I am a web developer.",
+  img: profile,
+  about: `Hello, I'm Fabien Battais. After a professional transition, I ventured into the field of web development. I pursued a Bac+2 program to achieve this goal.`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/fabien-battais-870944224/",
+  github: "https://github.com/GalaxyUnic0rn",
+  twitter: "https://twitter.com/",
+  instagram: "https://www.instagram.com/",
+};
+
+// Enter your Work Experience here
+/*export const workDetails = [
+  {
+    Position: "Frontend Web Developer",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Full Time",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+  {
+    Position: "Internship",
+    Company: `Company Name here`,
+    Location: "Bengaluru",
+    Type: "Internship",
+    Duration: "Sep 2021 - Dec 2021",
+  },
+];
+*/
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "Frontend and backend Development",
+    Company: "OpenClassroom",
+    Location: "Online",
+    Type: "Full Time",
+    Duration: "nov 2023 - Present",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  sass: sass,
+  tailwind: tailwind,
+  bootstrap: bootstrap,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  figma: figma,
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Booki",
+    image: projectImage1,
+    description: `csite qui permet au usager de trouver des hébergements et des activités dans la ville de leur choix`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://galaxyunic0rn.github.io/booki-projet/",
+    githubLink: "https://github.com/GalaxyUnic0rn/booki-projet",
+  },
+  {
+    title: "Portfolio architecte ",
+    image: projectImage2,
+    description: `Portfolio d'une architecte d'interieur (Sophie Bluel)`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "MenuMaker by Qwenta",
+    image: projectImage3,
+    description: `planifier le développement du site web "Menu Maker"`,
+    techstack: "notion",
+    previewLink: "https://www.figma.com/design/Q6NEUPqwz1U3HFaCaVoF7N/Maquette-desktop---Menu-Maker-by-Qwenta?node-id=0-6&t=BZrCriWjBug7fc7F-0",
+    githubLink: "https://github.com/GalaxyUnic0rn/Menu-Maker-",
+  },
+  {
+    title: "Portfolio Nina Carducci photographe",
+    image: projectImage4,
+    description: `optimisation SEO et optimisation du code`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://galaxyunic0rn.github.io/nina-carducci/",
+    githubLink: "https://github.com/GalaxyUnic0rn/nina-carducci",
+  },
+  {
+    title: "Kasa",
+    image: projectImage5,
+    description: `creation d'une application web de location immobilière avec React`,
+    techstack: "HTML/CSS, JavaScript, React",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/GalaxyUnic0rn/projet-kasa",
+  },
+  {
+    title: "Mon vieux grimmoire",
+    image: projectImage6,
+    description: `Developper le back-end d'un site de notation de livre`,
+    techstack: "HTML/CSS, JavaScript , NodeJS",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com/GalaxyUnic0rn/projet-Mon-vieux-grimoire",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "fab.battais@gmail.com",
+  phone: "0613944949",
+};
